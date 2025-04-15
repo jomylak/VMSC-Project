30bcf27aacb0d715ff96ec2191f4fe70b1f5eb95
Santhosh Vaddi <91930012+santhoshv25@users.noreply.github.com>
2025-04-15 19:37:05 -0400

    Update recommendation_engine.py

--- backend/recommendation_engine.py
+++ backend/recommendation_engine.py
@@ -1,14 +1,12 @@
-import pandas as pd
+def recommend_schedule(df): # Temporary recommendation (Finds the averages of the attendances for each day (monday, tuesday, etc))
+    best_day = df.groupby('day_of_week')['Attendance'].mean().idxmax()
+    best_month = df.groupby('month')['Attendance'].mean().idxmax() # Also finds for each month
+    best_type = df.groupby('Event Type')['Attendance'].mean().idxmax() # Finds the best event type depending on attendance
 
-def load_event_attendance_data():
-    df = pd.read_csv("cleaned_attendance.csv")
-    df['Event Date'] = pd.to_datetime(df['Event Date'])
-    df['day_of_week'] = df['Event Date'].dt.day_name()
-    df['month'] = df['Event Date'].dt.hour
+    return {
+        "best_day": best_day,
+        "best_month": best_month,
+        "best_type": best_type
+    }
 
-    return df
-
-def load_company_data(): #Still need to add more
-    df = pd.read_csv("cleaned_company_visit.csv")
-    
-    return df
+def find_popular_companies(df_event, df_company): #To Do
