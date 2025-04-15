1506aa2520bd388639ab19e320ed362e0f2d2e0a
Santhosh Vaddi <91930012+santhoshv25@users.noreply.github.com>
2025-04-15 19:40:45 -0400

    Update load_data.py

--- backend/load_data.py
+++ backend/load_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_event_attendance_data():
-    df = pd.read_csv("cleaned_attendance.csv")
+    df = pd.read_csv("Cleaned_Event_Attendance.csv")
     df['Event Date'] = pd.to_datetime(df['Event Date'])
     df['day_of_week'] = df['Event Date'].dt.day_name()
     df['month'] = df['Event Date'].dt.hour
@@ -9,6 +9,6 @@ def load_event_attendance_data():
     return df
 
 def load_company_data(): # unfinished
-    df = pd.read_csv("cleaned_company_visit.csv")
+    df = pd.read_csv("Cleaned_Company_Visits.csv")
     
     return df
